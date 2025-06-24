52bfaf824e6f91e870d590564073e95598dc48ff
Ravi Kant <ravi.18teen@gmail.com>
2025-06-24 15:18:10 +0530

    Update intermediate sql.sql

--- 2.Analyzing Game Sales Across Regions and Platforms/intermediate sql.sql
+++ 2.Analyzing Game Sales Across Regions and Platforms/intermediate sql.sql
@@ -1,85 +1,91 @@
-/* Q1. Insert a new game with the title "Future Racing", genre 
-"Racing", release date "2024-10-01", and developer "Speed Studios".*/
+/* 1. Insert a new game with the title "Future Racing", genre "Racing", release date
+"2024-10-01", and developer "Speed Studios".*/
 
-select * from games;
+INSERT INTO games(GameTitle,Genre,ReleaseDate,Developer)
+VALUES('Future Racing', 'Racing', '2024-10-01', 'Speed Studios');
 
-insert into games(GameTitle,Genre,ReleaseDate,Developer)
-values('Future Racing','Racing','2024-10-01','Speed Studios');
+/* 2. Update the price of the game with GameID 2 on the PlayStation platform to 60.*/
 
-/* Q2. Update the price of the game with GameID 2 on the PlayStation platform to 60.*/
+UPDATE game_sales 
+SET 
+    Price = 60
+WHERE
+    GameID = 2 AND Platform = 'PlayStation';
+    
+/* 3. Delete the record of the game with GameID 5 from the Game Sales table.*/
 
-select * from game_sales;
+DELETE FROM game_sales 
+WHERE
+    GameID = 5;
+    
+/* 4. Calculate the total number of units sold for each game across all platforms and
+regions.*/
 
-update game_sales
-set Price = 60
-where GameID = 2 and Platform = 'PlayStation';
+SELECT 
+    Platform, SalesRegion, SUM(UnitsSold) AS total_units
+FROM
+    game_sales
+GROUP BY Platform , SalesRegion
+ORDER BY total_units DESC;
 
-/* Q3. Delete the record of the game with GameID 5 from the Game Sales table.*/
+/* 5. Identify the game with the highest number of units sold in North America.*/
 
-select * from game_sales;
+SELECT 
+    Platform, SalesRegion, SUM(UnitsSold) AS total_units
+FROM
+    game_sales
+WHERE
+    SalesRegion = 'North America'
+GROUP BY SalesRegion , Platform
+ORDER BY total_units DESC;
 
-delete from game_sales where GameID = 5;
+/* 6. Get the game titles, platforms, and sales regions along with the units sold for each
+game.*/
 
-/* Q4. Calculate the total number of units sold for each 
-game across all platforms and regions. */
+SELECT 
+    g.GameTitle, gs.Platform, gs.SalesRegion, gs.UnitsSold
+FROM
+    game_sales gs
+        LEFT JOIN
+    games g ON gs.GameID = g.GameID;
 
-select Platform, SalesRegion, sum(UnitsSold) as total_units_sold
-from game_sales
-group by Platform,SalesRegion
-order by total_units_sold desc;
+/* 7. Find all games, including those that have no sales data in the Game Sales table.*/
 
-/* Q5. Identify the game with the highest number of units sold in North America.*/
+SELECT 
+    g.GameTitle, gs.Platform, gs.SalesRegion, gs.UnitsSold
+FROM
+    games g
+        LEFT JOIN
+    game_sales gs ON g.GameID = gs.GameID;
+    
+/* 8. Retrieve sales records where the game details are missing in the Games table.*/
 
-select *
-from game_sales
-where SalesRegion='North America'
-and UnitsSold = (select max(UnitsSold) from game_sales where SalesRegion='North America');
+SELECT 
+    gs.*
+FROM
+    games g
+        RIGHT JOIN
+    game_sales gs ON g.GameID = gs.GameID
+WHERE
+    g.GameID IS NULL;
 
-/* Q6. Get the game titles, platforms, and sales regions along
- with the units sold for each game.*/
- 
-SELECT g.GameTitle,
-gs.Platform,gs.SalesRegion,gs.UnitsSold
-FROM game_sales gs
-INNER JOIN games g ON gs.GameID = g.GameID
-ORDER BY g.GameTitle,gs.Platform,gs.SalesRegion,gs.UnitsSold;
-
-/* Q7. Find all games, including those that have no sales data 
-in the Game Sales table. */
-
-SELECT g.GameTitle,
-gs.Platform,gs.SalesRegion,gs.UnitsSold
-FROM games g
-LEFT JOIN game_sales gs ON g.GameID = gs.GameID
-
-/* Q8. Retrieve sales records where the game details are missing in the Games table.*/
-
-SELECT gs.GameID,gs.Platform,gs.SalesRegion,gs.UnitsSold
-FROM game_sales gs
-LEFT JOIN games g ON gs.GameID = g.GameID
-WHERE g.GameID IS NULL;
-
-/* Q9. Retrieve game sales data for North America and Europe removing duplicate
+/* 9. Retrieve game sales data for North America and Europe removing duplicate
 records.*/
--- METHOD - 1
-SELECT SalesRegion,
-       SUM(UnitsSold) AS TotalUnitsSold, 
-       ROUND(AVG(Price),2) AS AvgPrice
-FROM game_sales
-WHERE SalesRegion IN ('North America', 'Europe')
-GROUP BY SalesRegion;
 
--- METHOD - 2 
-SELECT DISTINCT SalesRegion
-FROM game_sales
-WHERE SalesRegion IN ('North America', 'Europe');
+SELECT DISTINCT
+    *
+FROM
+    game_sales
+WHERE
+    SalesRegion IN ('North America' , 'Europe');
 
-/* Q 10. Retrieve all game sales data from North America and Europe 
-without removing duplicate records.*/
+/* 10. Retrieve all game sales data from North America and Europe without removing
+duplicate records.*/
 
-SELECT GameID, Platform, SalesRegion, UnitsSold, Price
-FROM game_sales
-WHERE SalesRegion IN ('North America', 'Europe');
-
-
+SELECT 
+    *
+FROM
+    game_sales
+WHERE
+    SalesRegion IN ('North America' , 'Europe');
 
