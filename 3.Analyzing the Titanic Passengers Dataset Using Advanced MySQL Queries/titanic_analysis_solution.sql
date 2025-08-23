96ccf10b86cfbdbd8764cf6e997b754066ab13ea
Ravi Kant <ravi.18teen@gmail.com>
2025-08-23 09:02:39 +0530

    Update titanic_analysis_solution.sql

--- 3.Analyzing the Titanic Passengers Dataset Using Advanced MySQL Queries/titanic_analysis_solution.sql
+++ 3.Analyzing the Titanic Passengers Dataset Using Advanced MySQL Queries/titanic_analysis_solution.sql
@@ -1,7 +1,7 @@
 /* 1. Write a query to find the name and age of the oldest passenger who survived.*/
-select * from titanic;
+
 SELECT 
-    CONCAT(first_name, ' ', last_name) AS full_name, age, who
+    CONCAT(first_name, ' ', last_name) AS 'name', age, who
 FROM
     titanic
 WHERE
@@ -11,18 +11,12 @@ LIMIT 5;
 
 /* 2. Create a view to display passenger survival status, class, age, and fare.*/
 
-CREATE VIEW passenger_sur_info AS
+CREATE VIEW passenger_summary AS
     SELECT 
-        CONCAT(first_name, ' ', last_name) AS full_name,
-        survived,
-        class,
-        age,
-        fare
+        survived, class, age, fare
     FROM
         titanic;
-SELECT *
-FROM
-    passenger_sur_info;
+SELECT  * FROM passenger_summary;
 
 /* 3. Create a stored procedure to retrieve passengers based on a given age range.*/
 
@@ -35,80 +29,76 @@ BEGIN
 SELECT *
 FROM titanic
 WHERE age BETWEEN min_age AND max_age
-ORDER BY age ;
-end //
+ORDER BY age;
+END //
 DELIMITER ;
 CALL pass_by_age(20,40);
 
-/* 4. Write a query to categorize passengers based on the fare they paid: 'Low', 'Medium', or
-'High'.*/
+/* 4. Write a query to categorize passengers based on the fare they paid: 
+'Low', 'Medium', or 'High'. */
 
 SELECT
-Passenger_No,
-CONCAT(first_name,' ', last_name) AS full_name,
-age, who,fare,
-CASE NTILE(3) OVER (ORDER BY fare)
-WHEN 1 THEN 'Low'
+fare,
+CASE NTILE(3) OVER(ORDER BY fare DESC)
+WHEN 1 THEN 'High'
 WHEN 2 THEN 'Medium'
-WHEN 3 THEN 'HIGH'
-END AS fare_category
+WHEN 3 THEN 'Low'
+END AS fare_tier
 FROM titanic;
 
 /* 5. Show each passenger's fare and the fare of the next passenger.*/
 
-SELECT 
+SELECT
 Passenger_No,
 fare,
-LEAD(fare) OVER(ORDER BY Passenger_No) AS next_fare
+LEAD(fare) OVER(ORDER BY Passenger_No) AS 'lead'
 FROM titanic;
 
 /* 6. Show the age of each passenger and the age of the previous passenger.*/
 
 SELECT
 Passenger_No,
-CONCAT(first_name, ' ', last_name) AS full_name,
 age,
-LAG(age) OVER(ORDER BY Passenger_No) AS pre_age
+LAG(age) OVER(ORDER BY Passenger_No) AS 'lag'
 FROM titanic;
 
-/* 7. Write a query to rank passengers based on their fare, displaying rank for each passenger.*/
+/* 7. Write a query to rank passengers based on their fare, 
+displaying rank for each passenger.*/ 
 
 SELECT
 Passenger_No,
-CONCAT(first_name, ' ', last_name) AS full_name,
-age, fare,
-RANK() OVER (ORDER BY fare DESC) AS rnk
+fare,
+RANK() OVER(ORDER BY fare DESC) AS 'rank'
 FROM titanic;
 
 /* 8. Write a query to rank passengers based on their fare, ensuring no gaps in rank*/
 
 SELECT
 Passenger_No,
-CONCAT(first_name, ' ', last_name) AS full_name,
-age, fare,
-DENSE_RANK() OVER (ORDER BY fare DESC) AS d_rnk
+fare,
+DENSE_RANK() OVER(ORDER BY fare DESC) AS 'dense_rank'
 FROM titanic;
 
 /* 9. Assign row numbers to passengers based on the order of their fares.*/
 
 SELECT
 Passenger_No,
-CONCAT(first_name, ' ', last_name) AS full_name,
-age, fare,
-ROW_NUMBER() OVER (ORDER BY fare DESC) AS row_num
+fare,
+ROW_NUMBER() OVER(ORDER BY fare DESC) AS 'row_number'
 FROM titanic;
 
-/* 10. Use a CTE to calculate the average fare and find passengers who paid more than the
-average.*/
+/* 10. Use a CTE to calculate the average fare and find passengers 
+who paid more than the average.*/
 
-WITH avg_cte AS(
+WITH avgfare AS(
 SELECT
-AVG(fare) AS avg_fare
+AVG(fare) AS 'avg'
 FROM titanic
 )
-SELECT
+SELECT 
 t.Passenger_No,
-CONCAT(first_name,' ',last_name) AS name,
-t.fare, a.avg_fare
-FROM titanic t, avg_cte a
-WHERE t.fare > a.avg_fare;
+CONCAT(t.first_name,' ',t.last_name) AS 'name',
+t.fare,
+a.avg
+FROM titanic t, avgfare a
+WHERE t.fare > a.avg;
