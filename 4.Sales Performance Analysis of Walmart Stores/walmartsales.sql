1fca22955ed97be14d58037a8eea50633ad07b4a
Ravi Kant <ravi.18teen@gmail.com>
2025-08-27 08:09:48 +0530

    Update walmartsales.sql

--- 4.Sales Performance Analysis of Walmart Stores/walmartsales.sql
+++ 4.Sales Performance Analysis of Walmart Stores/walmartsales.sql
@@ -1,32 +1,35 @@
+ # Add a new date column in date format.
+ALTER TABLE walmart
+ADD COLUMN date_clean DATE;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE walmart
+SET date_clean = STR_TO_DATE(Date,'%d-%m-%Y');
+
 /* Task 1: Identifying the Top Branch by Sales Growth Rate 
 Walmart wants to identify which branch has exhibited the highest sales 
 growth over time. Analyze the total sales for each branch and compare 
 the growth rate across months to find the top performer.*/
 
-with monthly_sales AS(
-select
+WITH monthly_sales AS (
+SELECT 
 Branch,
-DATE_FORMAT(STR_TO_DATE(Date,'%d-%m-%Y'), '%m-%Y') AS month_year,
+DATE_FORMAT(date_clean,'%m-%Y') AS month_year,
 ROUND(SUM(Total)) AS total_amount
-FROM walmartsales
+FROM walmart
 GROUP BY Branch, month_year
 ),
 previous_sale AS(
 SELECT *,
-LAG(total_amount) OVER(PARTITION BY Branch ORDER BY month_year) AS previous_value
+LAG(total_amount) OVER(PARTITION BY Branch ORDER BY month_year) AS `lag` 
 FROM monthly_sales
 ),
 growth_value AS(
 SELECT *,
-((total_amount - previous_value)/ previous_value) * 100 AS growth_rate
+ROUND(((total_amount - `lag`)/ `lag`) * 100,2) AS growth_rate
 FROM previous_sale
-WHERE previous_value IS NOT NULL
+WHERE `lag` IS NOT NULL
 )
-SELECT
-Branch,
-month_year,
-total_amount,
-growth_rate
+SELECT * 
 FROM growth_value
 ORDER BY growth_rate DESC
 LIMIT 1;
@@ -36,46 +39,50 @@ Walmart needs to determine which product line contributes the highest profit to 
 The profit margin should be calculated based on the difference between the gross income
  and cost of goods sold.*/
  
- WITH highest_profit AS(
+ # gross income = profit (already given).
+ WITH product_profit AS(
  SELECT
  Branch,
  `Product line` AS product_line,
- ROUND(SUM(`gross income`)) AS profit_margin
- FROM walmartsales
+ ROUND(SUM(`gross income`),2) AS profit
+ FROM walmart
  GROUP BY Branch, product_line
  ),
- ranked_profit AS(
+ rank_profit AS(
  SELECT *,
- ROW_NUMBER() OVER(PARTITION BY Branch ORDER BY profit_margin DESC) AS rnk
- FROM highest_profit
+ RANK() OVER(PARTITION BY Branch ORDER BY profit DESC) as 'rank'
+ FROM product_profit
  )
- SELECT * 
- FROM ranked_profit
- WHERE rnk = 1
- ORDER BY profit_margin DESC;
+ SELECT 
+ Branch,
+ product_line,
+ profit
+ FROM rank_profit
+ WHERE `rank` = 1
+ ORDER BY profit DESC;
  
  /* Task 3: Analyzing Customer Segmentation Based on Spending.
 Walmart wants to segment customers based on their average spending behavior. 
 Classify customers into three tiers: High, Medium, and Low spenders based on 
 their total purchase amounts.*/
 
-WITH customer_average_spending AS (
+WITH average_spending AS (
 SELECT 
 `Customer ID` AS customer_id,
-ROUND(AVG(Total)) AS average_amount
-FROM walmartsales
+ROUND(AVG(Total),2) AS avg_amount 
+FROM walmart
 GROUP BY customer_id
 ),
 tier_customer AS(
 SELECT *,
-CASE NTILE(3) OVER(ORDER BY average_amount)
+CASE NTILE(3) OVER(ORDER BY avg_amount)
 WHEN 1 THEN 'Low Spendors'
 WHEN 2 THEN 'Medium Spendors'
 ELSE 'High Spendors'
-END AS  spending_category
-FROM customer_average_spending
+END AS spending_tier
+FROM average_spending
 )
-SELECT * FROM  tier_customer;
+SELECT * FROM tier_customer;
 
 /* Task 4: Detecting Anomalies in Sales Transactions.
 Walmart suspects that some transactions have unusually high or low sales 
@@ -84,40 +91,41 @@ compared to the average for the product line. Identify these anomalies.*/
 # To identify outliers using the Interquartile Range (IQR)
 
 WITH product_sorted AS(
-SELECT
+SELECT 
 `Product line` AS product_line,
 Total,
-ROW_NUMBER() OVER(PARTITION BY `Product line` ORDER BY Total) AS row_num,
+ROW_NUMBER() OVER(PARTITION BY `Product line` ORDER BY Total) AS `row_number`,
 COUNT(*) OVER(PARTITION BY `Product line`) AS total_rows
-FROM walmartsales
+FROM walmart
 ),
 quartile AS(
-SELECT
+SELECT 
 product_line,
-MAX(CASE WHEN row_num = FLOOR(0.25 * total_rows) THEN Total END) AS Q1,
-MAX(CASE WHEN row_num = FLOOR(0.75 * total_rows) THEN Total END) AS Q3
+MAX(CASE WHEN `row_number` = FLOOR(0.25 * total_rows) THEN Total END) AS Q1,
+MAX(CASE WHEN `row_number` = FLOOR(0.75 * total_rows) THEN Total END) AS Q3
 FROM product_sorted
 GROUP BY product_line
 ),
-anomalies AS (
-SELECT
-w.Branch,
+anomalies AS(
+SELECT 
 w.`Product line`,
 w.Total,
 q.Q1,
 q.Q3,
-(q.Q3 - q.Q1) AS IQR,
-CASE WHEN w.Total < (q.Q1 - 1.5*(q.Q3 - q.Q1))
-OR Total > (Q3 + 1.5* (q.Q3 - q.Q1)) 
+(q.Q3 - q.Q1) as IQR,
+CASE WHEN w.Total < (q.Q1 - 1.5 * (q.Q3 - q.Q1))
+OR w.Total > (q.Q3 + 1.5 * (q.Q3 - q.Q1))
 THEN 'Anomaly'
-ELSE 'Normal' 
-END AS anomaly_status
-FROM walmartsales w
-JOIN quartile q on w.`Product line` = q.product_line
+ELSE 'Normal'
+END AS anomly_status
+FROM walmart w
+JOIN quartile q ON w.`Product line` = q.product_line
 )
-SELECT * 
+SELECT 
+`Product line`,
+Total
 FROM anomalies
-WHERE anomaly_status = 'Anomaly';
+WHERE anomly_status = 'Anomaly';
 
 /* Task 5: Most Popular Payment Method by City 
 Walmart needs to determine the most popular payment method in each 
@@ -127,66 +135,84 @@ WITH payment_counts AS(
 SELECT 
 City,
 Payment,
-COUNT(*) AS payment_count,
-ROW_NUMBER() OVER(PARTITION BY City ORDER BY COUNT(*) DESC) AS rnk
-FROM walmartsales
-GROUP BY City,Payment
-)
-SELECT * 
+COUNT(*) AS payment_count
+FROM walmart
+GROUP BY City, Payment
+),
+ranked_payment AS(
+SELECT
+City,
+Payment,
+payment_count,
+ROW_NUMBER() OVER(PARTITION BY City ORDER BY payment_count DESC) AS `rank`
 FROM payment_counts
-WHERE rnk = 1;
+)
+SELECT 
+City,
+Payment AS most_popular_payment 
+FROM ranked_payment
+WHERE `rank` = 1;
 
 /* Task 6: Monthly Sales Distribution by Gender
 Walmart wants to understand the sales distribution between male and 
 female customers on a monthly basis.*/
 
 SELECT 
-    Gender,
-    DATE_FORMAT(STR_TO_DATE(Date, '%d-%m-%Y'), '%m-%Y') AS sales_month,
-    ROUND(SUM(Total)) AS total_amount
+    DATE_FORMAT(date_clean, '%m-%Y') AS month_year,
+    ROUND(SUM(CASE
+                WHEN Gender = 'Male' THEN Total
+            END),
+            2) AS male_sales,
+    ROUND(SUM(CASE
+                WHEN Gender = 'Female' THEN Total
+            END),
+            2) AS female_sales
 FROM
-    walmartsales
-GROUP BY Gender , sales_month
-ORDER BY sales_month , total_amount;
+    walmart
+GROUP BY month_year
+ORDER BY month_year;
 
 /* Task7: Best Product Line by Customer Type
 Walmart wants to know which product lines are preferred by different 
 customer types(Member vs. Normal).*/
 
 WITH product_sale AS(
-SELECT 
-`Customer type` AS customer_type,
+SELECT
 `Product line` AS product_line,
-ROUND(SUM(Total)) AS total_sales
-FROM walmartsales
-GROUP BY customer_type, product_line
+`Customer type` AS customer_type,
+ROUND(SUM(Total),2) AS total_amount
+FROM walmart
+GROUP BY product_line,customer_type
 ),
-ranked AS(
+rnk AS(
 SELECT *,
-RANK() OVER(PARTITION BY customer_type ORDER BY total_sales DESC) AS rnk
+ROW_NUMBER() OVER(PARTITION BY customer_type ORDER BY total_amount DESC) AS `rank`
 FROM product_sale
 )
-SELECT * 
-FROM ranked
-WHERE rnk = 1;
+select 
+product_line,
+customer_type,
+total_amount
+from rnk
+where `rank` = 1;
 
 /* Task 8: Identifying Repeat Customers 
 Walmart needs to identify customers who made repeat purchases within a 
 specific time frame (e.g., within 30 days).*/
 
-WITH customer_purchase AS(
+WITH customer_orders AS(
 SELECT
 `Customer ID` AS customer_id,
-STR_TO_DATE(Date,'%d-%m-%Y') AS purchase_date,
-LEAD(STR_TO_DATE(Date,'%d-%m-%Y')) OVER(PARTITION BY `Customer ID` ORDER BY STR_TO_DATE(Date,'%d-%m-%Y')) AS next_purchase_date
-FROM walmartsales
+date_clean as order_date,
+LAG(date_clean) OVER(PARTITION BY `Customer ID` ORDER BY date_clean) AS prev_date
+FROM walmart
 )
 SELECT 
 customer_id,
 COUNT(*) AS repeat_purchase
-FROM customer_purchase
-WHERE next_purchase_date IS NOT NULL
-AND DATEDIFF(next_purchase_date, purchase_date) <= 30
+FROM customer_orders
+WHERE prev_date IS NOT NULL
+AND DATEDIFF(order_date,prev_date) <=30
 GROUP BY customer_id
 ORDER BY repeat_purchase DESC;
 
@@ -194,11 +220,11 @@ ORDER BY repeat_purchase DESC;
 Walmart wants to reward its top 5 customers who have generated the most sales Revenue.*/
 
 SELECT 
-    `Customer ID`, ROUND(SUM(Total)) AS amount
+    `Customer ID`, ROUND(SUM(Total), 2) AS total_amount
 FROM
-    walmartsales
-GROUP BY `Customer Id`
-ORDER BY amount DESC
+    walmart
+GROUP BY `Customer ID`
+ORDER BY total_amount DESC
 LIMIT 5;
 
 /* Task 10: Analyzing Sales Trends by Day of the Week
@@ -206,10 +232,9 @@ Walmart wants to analyze the sales patterns to determine which day of the week
 brings the highest sales.*/
 
 SELECT 
-    DAYNAME(STR_TO_DATE(Date, '%d-%m-%Y')) AS Day,
-    ROUND(SUM(Total)) AS amount
+    DAYNAME(date_clean) AS `dayname`,
+    ROUND(SUM(Total), 2) AS total_amount
 FROM
-    walmartsales
-GROUP BY Day
-ORDER BY amount DESC;
-
+    walmart
+GROUP BY `dayname`
+ORDER BY total_amount DESC;
